ff8e03b6008a471b7f205645bcee8c58af58446c
GAtomis <850680822@qq.com>
2023-02-02 00:36:20 +0800

    update heroSection

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@
  * @Author: GAtomis 850680822@qq.com
  * @Date: 2023-01-31 21:48:06
  * @LastEditors: GAtomis
- * @LastEditTime: 2023-02-01 19:07:59
- * @Description: 头部注释
+ * @LastEditTime: 2023-02-02 00:33:18
+ * @Description: main
  */
 import { useState } from 'react'
 import  Quote from '@/components/Quote'
-
+import HeroSection from './components/HeroSection'
+import PhoneModel from './components/PhoneModel'
 
 function App() {
 
   return (
     <div className="App">
       <Quote></Quote>
+      <HeroSection></HeroSection>
+      <PhoneModel></PhoneModel>
+      
     </div>
   )
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,20 +1,34 @@
+
 /*
  * @Author: GAtomis 850680822@qq.com
  * @Date: 2023-02-01 19:10:17
  * @LastEditors: GAtomis
- * @LastEditTime: 2023-02-01 20:42:23
+ * @LastEditTime: 2023-02-02 00:03:49
  * @Description: heroSection 滚动部分
  */
 import React from 'react'
 import { useNamespace } from '@/hooks/use-namespace'
-import ""
+import mp4link from '@/assets/video/Ink - 21536.mp4'
+import "./HeroSection.scss"
 function HeroSection() {
 
     const hs = useNamespace("HeroSection")
     return (
         <section className={hs.b()}>
-            <div>
+            <h1 className={hs.e("title")}>
+                iPhone 14 Pro Max
+
+            </h1>
+            <div className={hs.e("textContainer")}>
+                <span>so cold</span>
+                <span>sp bad</span>
             </div>
+            <div className={hs.e("videoContainer")}>
+                <video src={mp4link} autoPlay muted loop></video>
+
+
+            </div>
+
         </section>
     )
 }
